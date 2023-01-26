282bf222ab1e7b3b4f06f1ec81fa652a636b92a5
Matias Rodriguez <mat.rodri@gmail.com>
2023-01-26 01:29:25 -0300

    ft: added create and read methods

--- desafio1backend.js
+++ desafio1backend.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor(){
+    constructor(filename){
         this.products = [];
+        this.filename = filename
     }
  
     generadorIds = () => {
@@ -25,7 +28,7 @@ class ProductManager {
     addProduct = (title, description, price, thumbnail, code, stock  ) => {
         const id = this.generadorIds();
         if (!title || !description || !price || !thumbnail || !code || !stock){
-            console.error('Faltan datos para agregar el producto');
+            console.error('Faltan datos para agregar el producto'); 
             return;
         }
         if (this.products.find(e => e.code === code )) {
@@ -42,11 +45,45 @@ class ProductManager {
             stock,
         });
     }
+
+    createProduct = async(title, description,price, thumbnail, code, stock,) =>
+    {
+        let id
+        if (this.products.length === 0) id = 1
+        else id = this.products[this.products.length - 1].id + 1
+        this.products.push({id, title, description,price, thumbnail, code, stock}),
+        fs.writeFileSync(this.filename, JSON.stringify(this.products, null, 2))
+    }
+     
+    
 }
+    const catalogo = fs.readFileSync('products.json', 'utf-8');
+    // console.log(catalogo);
+
+async function desafio () {
+
+    const productManager = new ProductManager ('products.json');
+    await productManager.createProduct('vacio', 'corte para parrilla', 1200, 'http://www.ternerita.com/cortes/vacío.jpg', 101, 10);
+    await productManager.createProduct('costilla', 'corte para parrilla', 999, 'http://www.ternerita.com/cortes/costilla.jpg', 102, 10);
+    await productManager.createProduct('tomahawk', 'corte gourmet', 2000, 'http://www.ternerita.com/cortes/tomahawk.jpg', 103, 10);
+    await productManager.createProduct('Tbone', 'corte gourmet', 1500, 'http://www.laternerita.com/cortes/tbone.jpg', 104, 15)
+
+}
+
+
+desafio ()
+
+
+
+
+
+
 const productManager = new ProductManager ();
 productManager.addProduct('vacio', 'corte para parrilla', 1200, 'http://www.ternerita.com/cortes/vacío.jpg', 101, 10);
 productManager.addProduct('costilla', 'corte para parrilla', 999, 'http://www.ternerita.com/cortes/costilla.jpg', 102, 10);
 productManager.addProduct('tomahawk', 'corte gourmet', 2000, 'http://www.ternerita.com/cortes/tomahawk.jpg', 103, 10);
+// productManager.addProduct('perro cromado', 'corte gourmet', 2000, 'http://www.ternerita.com/cortes/tomahawk.jpg', 103, 10);
+// productManager.addProduct('tomahawk', 2000, 'http://www.ternerita.com/cortes/tomahawk.jpg', 104, 10);
 
 // console.log(productManager.getProducts());
-console.log(productManager.getProductById(2));
+// console.log(productManager.getProductById(6));
